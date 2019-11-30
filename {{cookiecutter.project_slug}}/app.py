df06b536f3779f60b59a78b44d9c8d1fe5d2fe5c
Devon Troedel <devon.troedel@gmail.com>
2019-11-30 16:32:28 -0600

    more work

--- {{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/app.py
@@ -2,21 +2,23 @@ from sys import argv
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 from views.home import home
 
 config_filename = argv[1] or "prod_settings.cfg"
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
+
 
 def create_app(config_file=config_filename):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
 
-    Bootstrap(app)
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
 
